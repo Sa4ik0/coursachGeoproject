efaa8959255cd39071ffebf32cd32fe8a7196796
Sachiko <65193697+Sa4ik0@users.noreply.github.com>
2023-11-07 18:35:21 +0300

    Delete calculator directory

--- calculator/script.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-    const area = parseFloat(document.getElementById('area').value);
-    const baseCost = parseFloat(document.getElementById('baseCost').value);
-    const complexity = parseFloat(document.getElementById('complexity').value);
-
-    const totalCost = area * baseCost * complexity;
-
-    document.getElementById('result').innerText = `Стоимость заказа: ${totalCost.toFixed(2)} в вашей валюте`;
-}
