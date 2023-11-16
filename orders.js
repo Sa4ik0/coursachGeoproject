4a2c6472ecc8a22c3b2fa18e6237c8f99e598946
stepan kovalev and sergei gaytylyan <powerpuffxle6123@gmail.ru>
2023-11-16 09:58:12 +0300

    Edit

--- orders.js
+++ orders.js
@@ -35,13 +35,83 @@ function renderTable(data) {
       row.classList.add('completed');
     }
 
-    // Добавляем ячейку для кнопки "Заказ выполнен" или "Вернуть заказ"
+    // Добавляем ячейкии
     let actionCell = document.createElement('td');
+    let editCell = document.createElement('td');
+
     let actionButton = document.createElement('button');
+    let editButton = document.createElement('button');
+
     actionButton.textContent = item.completed ? 'ГОТОВ' : 'НЕ ГОТОВ';
+    editButton.textContent = 'Редакт'
+
     actionButton.onclick = function() {
       toggleOrderStatus(item.id, !item.completed);
     };
+
+    editButton.onclick = () => {
+      editOrder(item.id);
+    };
+
+    actionCell.appendChild(actionButton);
+    editCell.appendChild(editButton);
+
+    row.appendChild(editCell);
+    row.appendChild(actionCell);
+
+    tableBody.appendChild(row);
+  });
+}
+
+function renderTable(data) {
+  let tableBody = document.getElementById('dataBody');
+  tableBody.innerHTML = '';
+
+  data.forEach(function(item) {
+    let row = document.createElement('tr');
+
+    let properties = ['id', 'price', 'phone', 'surname', 'name', 'lastname', 'town', 'size', 'hard'];
+    properties.forEach(function(prop) {
+      let cell = document.createElement('td');
+
+      // Создаем редактируемый элемент
+      let editableElement = document.createElement('input');
+      editableElement.value = item[prop];
+      editableElement.readOnly = true; // по умолчанию не редактируемый
+
+      editableElement.ondblclick = function() {
+        // Двойной клик для начала редактирования
+        editableElement.readOnly = false;
+      }
+
+      editableElement.onblur = function() {
+        // Потеря фокуса для сохранения изменений
+        editableElement.readOnly = true;
+        updateOrderProperty(item.id, prop, editableElement.value);
+      }
+
+      editableElement.onkeypress = function(e) {
+        // Нажатие Enter для сохранения изменений
+        if (e.key === 'Enter') {
+          editableElement.readOnly = true;
+          updateOrderProperty(item.id, prop, editableElement.value);
+        }
+      }
+
+      cell.appendChild(editableElement);
+      row.appendChild(cell);
+    });
+
+    let actionCell = document.createElement('td');
+
+    let actionButton = document.createElement('button');
+
+    actionButton.textContent = item.completed ? 'ГОТОВ' : 'НЕ ГОТОВ';
+
+    actionButton.onclick = function() {
+      toggleOrderStatus(item.id, !item.completed);
+    };
+
     actionCell.appendChild(actionButton);
     row.appendChild(actionCell);
 
@@ -49,12 +119,36 @@ function renderTable(data) {
   });
 }
 
+
 function toggleOrderStatus(orderId, newStatus) {
   // Обновляем статус заказа на сервере
   updateOrderStatusOnServer(orderId, newStatus);
 
   // Обновляем локальные данные и интерфейс
   fetchData();
+}
+
+function updateOrderProperty(orderId, property, value) {
+  fetch(`http://localhost:3000/updateOrderProperty/${orderId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ property: property, value: value }),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Order property updated:', data);
+    // При необходимости обновите данные в таблице или уведомите пользователя
+  })
+  .catch(error => {
+    console.error('Error updating order property:', error);
+  });
 }
 
 function updateOrderStatusOnServer(orderId, newStatus) {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
@@ -10,10 +11,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*'); // Разрешить все источники (для разработки)
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
+});
+
+app.use(cors());
+
+app.put('/updateOrderProperty/:orderId', (req, res) => {
+  const orderId = req.params.orderId;
+  const { property, value } = req.body;
+
+  fs.readFile('orders.json', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+      return;
+    }
+
+    const orders = JSON.parse(data);
+    const order = orders.find(order => order.id === orderId);
+
+    if (!order) {
+      res.status(404).send('Order not found');
+      return;
+    }
+
+    // Обновляем свойство заказа
+    order[property] = value;
+
+    fs.writeFile('orders.json', JSON.stringify(orders, null, 2), (err) => {
+      if (err) {
+        res.status(500).send('Error writing file');
+        return;
+      }
+
+      res.status(200).json({ message: 'Order property updated successfully' });
+    });
+  });
 });
 
 // Загрузка заказов
@@ -53,7 +88,6 @@ app.post('/saveOrder', (req, res) => {
   });
 });
 
-// Обновление статуса заказа
 app.put('/updateOrderStatus/:orderId', (req, res) => {
   const orderId = req.params.orderId;
   const newStatus = req.body.status;
